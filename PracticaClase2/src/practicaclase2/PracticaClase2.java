478f89406673031009705bb2bec8fdd620830532
MaripazFajardo <mfajardo20737@ufide.ac.cr>
2026-02-17 21:14:37 -0600

    Add files via upload

---
+++ PracticaClase2/src/practicaclase2/PracticaClase2.java
@@ -0,0 +1,218 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practicaclase2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author marip
+ */
+public class PracticaClase2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Preguntar al usuario los años de antigüedad
+        // Cantidad de horas laboradas x semana
+        //Precio que se le paga x hora
+        //Calcular el salario bruto 
+        
+//       int antiguedad;
+//           antiguedad = Integer.parseInt(JOptionPane.showInputDialog("Digite sus años de antiguedad"));
+         //Cantidad de perros que hay en una tienda
+         //Precio de un bulto de comida
+         //Nombre del gato más famoso de la timeda
+         //Letra de la sección de donde esta el animal
+//         int perros;
+//         double precio;
+//         String gato;
+//         char seccion;
+       
+//        
+//         int puntos = 100;
+//         double estatura = 1.75;
+
+//String jugador = "ProGamer";
+//System.out.println("El nombre del jugador es: " + jugador);
+
+//String nombre = "luis"; //Luis entre comilla puesto a que es un texto
+//int edad = 20;
+//System.out.println("Hola " + nombre + " tienes " + edad + " años."); //Poner espacio despues de las comillas
+
+//String lectura;
+//int edad;
+//double salario;
+//String nombre;
+//
+//lectura = JOptionPane.showInputDialog("Digite su edad:");
+//edad = Integer.parseInt(lectura);
+//nombre = JOptionPane.showInputDialog("Digite su nombre");
+//
+//lectura = JOptionPane.showInputDialog("Digite su salario:");
+//salario = Double.parseDouble(lectura);
+//
+//JOptionPane.showMessageDialog(null,"El nombre de la persona es: " + nombre + " y tiene: " + edad + " años. Recibe un salario de: " +salario);
+//
+//JOptionPane.showMessageDialog(null, "Bienvenido al mundo de Java. Podrás dar solución a muchos problems");
+//Programa que calcule la suma y el promedio de 4 numeros enters
+    
+
+// int n1 = 2;
+// int n2 = 8;
+// int n3 = 6;
+// int n4 = 4;
+// 
+// int suma = n1 + n2 + n3 + n4;
+// double promedio = suma / 4.0;
+//  JOptionPane.showMessageDialog(null, "La suma es: " + suma + " Su promedio es: " + promedio);
+      
+//   String lectura;
+//   int n1, n2, n3, n4, suma;
+//   double promedio;
+//   
+//   lectura = JOptionPane.showInputDialog("Digite el primer número:");
+//        n1 = Integer.parseInt(lectura);
+//
+//        lectura = JOptionPane.showInputDialog("Digite el segundo número:");
+//        n2 = Integer.parseInt(lectura);
+//
+//        lectura = JOptionPane.showInputDialog("Digite el tercer número:");
+//        n3 = Integer.parseInt(lectura);
+//
+//        lectura = JOptionPane.showInputDialog("Digite el cuarto número:");
+//        n4 = Integer.parseInt(lectura);
+//
+//        // 3. Hacemos los cálculos
+//        suma = n1 + n2 + n3 + n4;
+//        promedio = suma / 4.0; // Usamos 4.0 para que Java entienda que queremos decimales
+//        // 4. Mostramos los resultados
+//        JOptionPane.showMessageDialog(null, "Resultados:\n" + //esa "\n" sirve para hacer un salto de linea, como un enter para que los resultados no salgan pegados
+//                "Suma: " + suma + "\n" +
+//                "Promedio: " + promedio);
+
+//JOptionPane.showMessageDialog(null, "Hola María, bienvenida a este programa desarrollado en JAVA con netbeans");
+
+//String lectura;
+//int edad;
+//int promedio;
+//        lectura = JOptionPane.showInputDialog("Digite su edad:");
+//        edad = Integer.parseInt(lectura);
+//        promedio = 5 + edad;
+//        JOptionPane.showMessageDialog(null, "Dentro de 5 años tendra: " + promedio);
+//        
+// Monto de ingreso mensual
+// Gasto mensual por alimentación.
+// Calcular el porcentaje de gastos x concepto de alimentación
+// porcentaje que queda disponible para otros gastos 
+
+//String lectura;
+//double ingreso, gastoAlimentos, porcentajeAlimentos, porcentajeOtros;
+// lectura =  JOptionPane.showInputDialog("Digite su ingreso mensual:");
+// ingreso = Double.parseDouble(lectura);
+// 
+//   lectura = JOptionPane.showInputDialog("Ingrese sus gastos en los alimentos");
+//   gastoAlimentos = Double.parseDouble(lectura);
+//   porcentajeAlimentos = (gastoAlimentos / ingreso)*100;
+//   porcentajeOtros = 100 - porcentajeAlimentos;
+//   
+//   JOptionPane.showMessageDialog(null, "El porcentaje de alimentación: " + porcentajeAlimentos + "%" +
+//           "\nPorcentaje disponible para otros gastos: " +porcentajeOtros + "%");
+
+//  CAEscriba un nuevo programa en JAVAque le solicite al usuario su edad y le indique si puede o no puede votar
+//String lectura;
+//int edad;
+//lectura = JOptionPane.showInputDialog("Digite su edad");
+//edad = Integer.parseInt(lectura);
+//if (edad >= 18) {
+//   JOptionPane.showMessageDialog(null, "Puede votar"); 
+//}else{
+//     JOptionPane.showMessageDialog(null, "No puede votar"); 
+//}
+//   String lectura;
+//   int nota;
+//   lectura = JOptionPane.showInputDialog("Digite su nota final");
+//   nota = Integer.parseInt(lectura);
+//   if (nota >= 70) {
+//       JOptionPane.showConfirmDialog(null, "Aprobaste el curso");
+//   }else{
+//       JOptionPane.showMessageDialog(null, "Reprobaste");
+//int edad;
+//String lectura;
+//lectura = JOptionPane.showInputDialog("Digite su edad");
+//edad = Integer.parseInt(lectura);
+//        
+//if (edad >= 18) {
+//    System.out.println("Puede votar");
+//    if (edad >= 31) {
+//        System.out.println("Puede ser presidente");
+//Imagina que para entrar a ver una película de terror, necesitas:
+//
+//Tener al menos 18 años.
+//Tener tu entrada comprada.
+
+//int edad;
+//String entrada, lectura;
+//
+//lectura = JOptionPane.showInputDialog("Digite su edad");
+//edad = Integer.parseInt(lectura);
+//entrada = JOptionPane.showInputDialog("Compraste tu entrada?");
+//if (edad >=18 ) {
+//    if (entrada.equalsIgnoreCase("si")){
+//        JOptionPane.showMessageDialog(null, "Bienvenido al cine");
+//        
+//    }else { 
+//        JOptionPane.showMessageDialog(null, "Tienes que comprar tu entrada");
+//    }
+//    
+//    }else {
+//    JOptionPane.showMessageDialog(null,"Eres menor de edad, no puedes entrar");
+//    
+//}
+//Se le solicita al usuario el número de dia
+// Se le indica el nombre del dia
+int numero;
+String lectura, dia;
+lectura = JOptionPane.showInputDialog("Digite el numero de dia");
+numero = Integer.parseInt(lectura);
+
+switch (numero){
+    case 1: 
+        dia = "Dimanche";
+        break;
+    case 2: 
+        dia = "Lundi";
+        break;
+    case 3: 
+        dia = "Mardi";
+        break;    
+
+    case 4: 
+        dia = "Mercredi";
+        break;   
+        
+        case 5: 
+        dia = "Jeundi";
+        break;
+     case 6: 
+        dia = "Vendredi";
+        break;
+    case 7: 
+        dia = "Samedi";
+        break;
+    default:
+        dia = "Numero no valido (debe ser del uno al siete)";
+        break;
+}
+JOptionPane.showConfirmDialog(null, "El dia es: " + dia);
+
+    }
+}
+   
+   
+
